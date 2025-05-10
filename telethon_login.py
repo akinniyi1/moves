a938ab9d622725de651aed89f0aaec0207084061
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-05-10 07:14:25 +0100

    Update telethon_login.py

--- telethon_login.py
+++ telethon_login.py
@@ -1,17 +1,1 @@
-import os
-import asyncio
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
 
-api_id = int(os.getenv("TELETHON_API_ID"))
-api_hash = os.getenv("TELETHON_API_HASH")
-
-async def main():
-    print("Starting login...")
-    async with TelegramClient(StringSession(), api_id, api_hash) as client:
-        await client.start()
-        print("Login successful.")
-        print("Copy and save the following session string:")
-        print(client.session.save())
-
-asyncio.run(main())
